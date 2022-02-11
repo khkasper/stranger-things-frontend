a7941058f82a03f1ca9dd8148f655cf1acb1d5fd
Kristiano <kristianokasper@gmail.com>
2022-02-11 18:32:54 -0300

    Em produçao

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <h1> Em desenvolvimento </h1>
       <StrangerThings />
     </div>
   );
